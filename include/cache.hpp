4fd0aa78f1a6aecedc225d7f6810e24e4a02cc7b
Atlaxsyys <y.shafran-06@mail.ru>
2025-09-24 02:41:59 +0300

    added get and put func and started cmake

--- include/cache.hpp
+++ include/cache.hpp
@@ -3,10 +3,13 @@
 #include <iostream>
 #include <list>
 #include <unordered_map>
+#include <stdexcept>
 
-template<typename Value, typename Key>
+namespace cache {
 
-class 2Q_cache {
+template<typename Key, typename Value>
+
+class TwoQCache {
 private:
     using ListIterator = typename std::list<Key>::iterator;
 
@@ -18,17 +21,15 @@ private:
     std::list<Key> A1out_;
     std::list<Key> Am_;
 
-    std::unordered_map<Key, Value> values;  // key -> value (only in cache)
-    std::unordered_map<Value, ListIterator> iterators;  // key -> position
-    std::unordered_map<Key, char> types;  // 'i' - A1in, 'm' - Am, 'o' - A1out
+    std::unordered_map<Key, Value> values_;           // key -> value (only in cache)
+    std::unordered_map<Key, ListIterator> iterators_; // key -> position
+    std::unordered_map<Key, char> types_;            // 'i' - A1in, 'm' - Am, 'o' - A1out
 
-    
-        void reclaimFor(const Key& key)
-        {
-
+    void reclaimFor(const Key& key)
+    {
         if (values_.size() < capacity_)
         {
-            return; // Free slot available
+            return;
         }
 
         if (A1in_.size() > kin_)
@@ -46,11 +47,13 @@ private:
                 A1out_.pop_back();
                 types_.erase(out_tail);
             }
+
             A1out_.push_front(evict_key);
             types_[evict_key] = 'o';
             iterators_[evict_key] = A1out_.begin();
+
         }
-        
+
         else
         {
             // Evict from Am
@@ -61,4 +64,111 @@ private:
             types_.erase(evict_key);
         }
     }
+
+public:
+    TwoQCache(std::size_t capacity, std::size_t kin = 0, std::size_t kout = 0)
+        : capacity_(capacity), 
+          kin_(kin == 0 ? capacity / 4 : kin),
+          kout_(kout == 0 ? capacity / 2 : kout)
+    {
+        if (capacity_ == 0)   { throw std::invalid_argument("Cache capacity must be greater than 0");  }
+        if (kin_ > capacity_) { throw std::invalid_argument("A1in size (kin) cannot exceed capacity"); }
+    }
+
+    bool get(const Key& key, Value& value)
+    {
+        auto type_it = types_.find(key);
+        
+        if (type_it == types_.end())
+        {
+            return false; // Key not found
+        }
+
+        char type = type_it->second;
+        
+        if (type == 'm')
+        {
+            // Move to front of Am
+            Am_.erase(iterators_[key]);
+            Am_.push_front(key);
+            iterators_[key] = Am_.begin();
+            value = values_[key];
+            return true;
+        }
+
+        else if (type == 'i')
+        {
+            // Stay in A1in
+            value = values_[key];
+            return true;
+        }
+        
+        else if (type == 'o')
+        {
+            // Promote from A1out to Am
+            reclaimFor(key);
+            A1out_.erase(iterators_[key]);
+            types_.erase(key);
+            Am_.push_front(key);
+            iterators_[key] = Am_.begin();
+            types_[key] = 'm';
+            return false; // Value not in cache yet
+        }
+        return false;
+    }
+
+    void put(const Key& key, const Value& value)
+    {
+        auto type_it = types_.find(key);
+
+        if (type_it != types_.end())
+        {
+            char type = type_it->second;
+
+            if (type == 'm')
+            {
+                // Update and move to front of Am
+                Am_.erase(iterators_[key]);
+                Am_.push_front(key);
+                iterators_[key] = Am_.begin();
+            }
+
+            else if (type == 'i')
+            {
+                // Stay in A1in
+            }
+            
+            else if (type == 'o')
+            {
+                // Promote to Am
+                reclaimFor(key);
+                A1out_.erase(iterators_[key]);
+                Am_.push_front(key);
+                iterators_[key] = Am_.begin();
+                types_[key] = 'm';
+            }
+
+            values_[key] = value;
+            return;
+        }
+
+        // New key
+        reclaimFor(key);
+        A1in_.push_front(key);
+        iterators_[key] = A1in_.begin();
+        types_[key] = 'i';
+        values_[key] = value;
+    }
+
+    std::size_t size() const
+    {
+        return values_.size();
+    }
+
+    bool empty() const
+    {
+        return values_.empty();
+    }
+};
+
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,25 @@
+#include "../include/cache.hpp"
 #include <iostream>
 
-
-int main ()
+int main()
 {
+    cache::TwoQCache<int, std::string> cache(4); // create cache: Key=int, Value=string, capacity=4 (kin=1, kout=2)
+    cache.put(1, "one");   // added {1: "one"} в A1in
+    cache.put(2, "two");
+
+    std::string value;
+
+    if (cache.get(1, value)) { std::cout << "Key 1: " << value << std::endl; }
+
+    cache.put(3, "three");
+    cache.put(4, "four");
+    cache.put(5, "five");
+
+    if (!cache.get(2, value)) { std::cout << "Key 2 not found" << std::endl; }
+
+    cache.put(2, "two again");
+
+    std::cout << "Cache size: " << cache.size() << std::endl;
+
     return 0;
 }
